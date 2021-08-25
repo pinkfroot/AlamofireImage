f952bee17329e11c517bcf7bab06ac225ea868fd
Nial Giacomelli <nial@pinkfroot.com>
2021-08-25 17:10:03 +0100

    Bump iOS dep

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@
 import PackageDescription
 
 let package = Package(name: "AlamofireImage",
-                      platforms: [.iOS(.v10),
+                      platforms: [.iOS(.v12),
                                   .macOS(.v10_12),
                                   .tvOS(.v10),
                                   .watchOS(.v3)],
